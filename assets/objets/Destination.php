dd2717b0de6dc13a43b8a185d0e6b344274830a4
InovaXion <mmghaoues@gmail.com>
2019-06-03 16:24:15 +0200

    update

--- assets/objets/Destination.php
+++ assets/objets/Destination.php
@@ -7,10 +7,17 @@ class Destination
     private $location;
     private $price;
     private $id_tour_operator;
+    private $imgPath;
 
-    public function __construct($location)
+    public function __construct($id,$location,$price,$id_tour_operator,$imgPath)
     {
+        $this->id = $id;
         $this->location = $location;
+        $this->price = $price;
+        $this->id_tour_operator = $id_tour_operator;
+        $this->imgPath = $imgPath;
+
+
     }
 
     public function getId()
@@ -23,10 +30,16 @@ class Destination
     }
     public function getPrice()
     {
+        return $this->price;
         
     }
     public function getId_tour_operator()
     {
+        return $this->id_tour_operator;
         
+    }
+    public function getImgPath()
+    {
+        return $this->imgPath;
     }
 }
--- pages/user/index.php
+++ pages/user/index.php
@@ -1,53 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ComparOperator</title>
-    <?php require('../../partials/links.php'); ?>
-    <?php // Connexion bdd
-$db = new PDO('mysql:host=127.0.0.1;dbname=ComparOperator', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué. ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ComparOperator</title>
+  <?php require('../../partials/links.php'); ?>
+  <?php require('../../assets/bdd/bdd.php'); ?>
+
 
 </head>
+
 <body>
-    
-<?php include('../../partials/header.php'); ?>
+
+  <?php include('../../partials/header.php'); ?>
 
 
 
 
 
-<div class="card text-center" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Le domaine de Ker-Ys</h5>
-    <img src="https://www.camping-ledomainedekerys.fr/wp-content/uploads/sites/14/2016/04/camping-st-nic-vue-aerienne.jpg" class="img-fluid" alt="Responsive image">
-    <p class="card-text">Une situation exceptionnelle dans la baie de Douarnenez ! </p>
-    <a href="#" class="btn btn-primary">Voir l'offre</a>
+  <div class="card text-center" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Le domaine de Ker-Ys</h5>
+      <img src="https://www.camping-ledomainedekerys.fr/wp-content/uploads/sites/14/2016/04/camping-st-nic-vue-aerienne.jpg" class="img-fluid" alt="Responsive image">
+      <p class="card-text">Une situation exceptionnelle dans la baie de Douarnenez ! </p>
+      <a href="#" class="btn btn-primary">Voir l'offre</a>
+    </div>
   </div>
-</div>
 
 
 
-<?php include('../../assets/objets/Manager.php'); 
-      include('../../assets/objets/Destination.php');
+  <?php include('../../assets/objets/Manager.php');
+  include('../../assets/objets/Destination.php');
 
-$manager = new Manager($db);
-$test = $manager->getAllDestination();
+  $manager = new Manager($db);
+  $test = $manager->getAllDestination();
 
-echo '<pre>' . var_export($test, true) . '</pre>';
-
-
-foreach ($test as $test)
-{
-  $destination = New Destination($test['location']);
- echo $destination->getLocation();
-}
-?>
+  echo '<pre>' . var_export($test, true) . '</pre>';
 
 
-
+  foreach ($test as $test) {
+    $destinations = new Destination($test['id'], $test['location'], $test['price'], $test['id_tour_operator'], $test['imgPath']);
+    echo '<pre>' . var_export($destinations, true) . '</pre>';
+    
+  }
+  ?>
 
 
 
@@ -57,9 +55,13 @@ foreach ($test as $test)
 
 
 
-<?php include('../../partials/footer.php'); ?>
+
+
+
+  <?php include('../../partials/footer.php'); ?>
 
 
 </body>
 <?php require('../../partials/scripts.php'); ?>
+
 </html>
