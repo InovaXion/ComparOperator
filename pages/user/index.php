179378564580bfe4caf44460e5ffec68f619372b
elodyVIGNAL <elodievignal12@gmail.com>
2019-06-03 16:28:28 +0200

    update

--- pages/user/index.php
+++ pages/user/index.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ComparOperator</title>
-    <?php require('../../partials/links.php'); ?>
-    <?php // Connexion bdd
-$db = new PDO('mysql:host=127.0.0.1;dbname=ComparOperator', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué. ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ComparOperator</title>
+  <?php require('../../partials/links.php'); ?>
+  <?php // Connexion bdd
+  $db = new PDO('mysql:host=127.0.0.1;dbname=ComparOperator', 'root', '');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
+  ?>
 
 </head>
+
 <body>
-    
-<?php include('../../partials/header.php'); ?>
+
+  <?php include('../../partials/header.php'); ?>
 
 
 
 
 
-<div class="card text-center" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Le domaine de Ker-Ys</h5>
-    <img src="https://www.camping-ledomainedekerys.fr/wp-content/uploads/sites/14/2016/04/camping-st-nic-vue-aerienne.jpg" class="img-fluid" alt="Responsive image">
-    <p class="card-text">Une situation exceptionnelle dans la baie de Douarnenez ! </p>
-    <a href="#" class="btn btn-primary">Voir l'offre</a>
+  <div class="card text-center" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Le domaine de Ker-Ys</h5>
+      <img src="https://www.camping-ledomainedekerys.fr/wp-content/uploads/sites/14/2016/04/camping-st-nic-vue-aerienne.jpg" class="img-fluid" alt="Responsive image">
+      <p class="card-text">Une situation exceptionnelle dans la baie de Douarnenez ! </p>
+      <a href="toursOpForDestination.php" class="btn btn-primary">Voir l'offre</a>
+    </div>
   </div>
-</div>
 
 
 
-<?php include('../../assets/objets/Manager.php'); 
-      include('../../assets/objets/Destination.php');
+  <?php include('../../assets/objets/Manager.php');
+  include('../../assets/objets/Destination.php');
 
-$manager = new Manager($db);
-$test = $manager->getAllDestination();
+  $manager = new Manager($db);
+  $test = $manager->getAllDestination();
 
-echo '<pre>' . var_export($test, true) . '</pre>';
+  echo '<pre>' . var_export($test, true) . '</pre>';
 
 
-foreach ($test as $test)
-{
-  $destination = New Destination($test['location']);
- echo $destination->getLocation();
-}
-?>
-
+  foreach ($test as $test) {
+    $destination = new Destination($test['location']);
+    echo $destination->getLocation();
+  }
+  ?>
 
 
 
@@ -57,9 +58,11 @@ foreach ($test as $test)
 
 
 
-<?php include('../../partials/footer.php'); ?>
+
+  <?php include('../../partials/footer.php'); ?>
 
 
 </body>
 <?php require('../../partials/scripts.php'); ?>
+
 </html>
