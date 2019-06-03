80b26588083b5d20a594512b851c3956206072ca
InovaXion <mmghaoues@gmail.com>
2019-06-03 09:09:15 +0200

    test

---
+++ assets/objets/tourOperator.php
@@ -0,0 +1,36 @@
+<?php 
+
+class tourOperator
+{
+    private $id;
+    private $name;
+    private $grade;
+    private $link;
+    private $is_premium;
+
+    function __construct()
+    {
+        
+    }
+
+    function getId()
+    {}
+    
+    function getName()
+    {}
+
+    function getGrade()
+    {}
+
+    function getLink()
+    {}
+
+    function getIsPremium()
+    {}
+
+
+
+
+
+
+}
