bd53a2a967a0395c534d883cc8216a2cc226851f
InovaXion <mmghaoues@gmail.com>
2019-06-05 12:01:04 +0200

    update

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <div class="header">
 
-    <a href="#" class="logo"><img id="logo" src="../../assets/images/logo.png" alt="logo"></a>
+    <a href="../../../../pages/user" class="logo"><img id="logo" src="../../assets/images/logo.png" alt="logo"></a>
 
 
     <div class="header-right">
