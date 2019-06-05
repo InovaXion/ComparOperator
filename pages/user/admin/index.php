d687538967f7ddcf4573f14710d82266402c0a26
InovaXion <mmghaoues@gmail.com>
2019-06-05 15:36:12 +0200

    update

--- pages/user/admin/index.php
+++ pages/user/admin/index.php
@@ -21,7 +21,7 @@
 <body>
 
     <?php
-    include('../../../partials/header.php');
+    
     // Les objets  //
     include('../../../assets/objets/Manager.php');
     include('../../../assets/objets/Destination.php');
@@ -35,6 +35,28 @@
     $to = $manager->getAllOperator();
     echo '<pre>' . var_export($to, true) . '</pre>';
     ?>
+<div class="header">
+
+<a href="index.php" class="logo"><img id="logo" src="../../../assets/images/logo.png" alt="logo"></a>
+
+
+<div class="header-right">
+    <h1>Compare Operator </h1>
+    <a href="index.php">Accueil</a>
+    <a href="#messages">Messages</a>
+    <a href="#profil">Profil</a>
+    <a href="#inscription">S'inscrire</a>
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="recherche" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Rechercher</button>
+    </form>
+
+</div>
+</div>
+
+<div style="padding-left:20px">
+
+</div>
 
     <h1>Session Administrateur</h1>
 
