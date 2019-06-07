b3882b4c531641cc7df2eab86495ececdd0f0ec2
InovaXion <mmghaoues@gmail.com>
2019-06-07 15:00:21 +0200

    update

--- pages/user/admin/sessionAdministrateur.php
+++ pages/user/admin/sessionAdministrateur.php
@@ -45,7 +45,7 @@
             <a href="../index.php">Accueil</a>
             <a href="#messages">Messages</a>
             <a href="#profil">Profil</a>
-            <a href="#inscription">S'inscrire</a>
+            <a href="#"></a>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="recherche" aria-label="Search">
                 <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Rechercher</button>
